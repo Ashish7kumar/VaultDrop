ce32fbde891691d3d552eca313dc60a0b3979304
unknown <ashish11to12@gmail.com>
2025-05-06 13:05:18 +0530

    Added Rate Limiter Dos Attack

---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,12 @@
+import rateLimit from "express-rate-limit";
+export const secretKeyRateLimiter=rateLimit({
+    windowMs:1*60*1000,
+    max:3,
+    message:'Too many attempts plese try after 60 seconds'
+
+})
+export const fileSendingRateLimiter=rateLimit({
+    windowMs:1*60*1000,
+    max:7,
+    message:'Too many request from your side,plese send file after 60 seconds'
+})
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import uploadController from "../controller/upload.controller";
-
+import { fileSendingRateLimiter } from "../middleware/rateLimiter";
 import upload from "../middleware/upload";
 const apiRouter=Router();
 
-apiRouter.post('/upload/:timeLimit',upload.single('file'),uploadController);
+apiRouter.post('/upload/:timeLimit',fileSendingRateLimiter,upload.single('file'),uploadController);
 
 export default apiRouter;
